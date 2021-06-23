6e70cde982477dbd45c70219e0f61e4578e48b16
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-06-23 22:42:05 +0100

    implementing the popup modal and added icons needed for buttons

--- jsfiles/index.js
+++ jsfiles/index.js
@@ -3,14 +3,49 @@ const closeButton = document.querySelector('.btn-x');
 const listItem = document.querySelectorAll('.list-item');
 const dropdown = document.getElementById('drop-down');
 function toggleElement() {
-  if (dropdown.style.display === 'none') {
-    dropdown.style.display = 'flex';
-  } else {
-    dropdown.style.display = 'none';
-  }
+	if (dropdown.style.display === 'none') {
+		dropdown.style.display = 'flex';
+	} else {
+		dropdown.style.display = 'none';
+	}
 }
 for (let i = 0; i < listItem.length; i++) {
-  listItem[i].addEventListener('click', toggleElement);
+	listItem[i].addEventListener('click', toggleElement);
 }
 hamburger.addEventListener('click', toggleElement);
 closeButton.addEventListener('click', toggleElement);
+
+
+
+
+
+
+
+
+
+
+let messsage = [];
+projectDetails = (e) => {
+	e.preventDefault
+	const popUpText = document.createElement('p');
+}
+
+const seeProject = document.querySelectorAll('.project');
+for (let i = 0; i < seeProject.length; i++) {
+	seeProject[i].addEventListener('click', projectDetails);
+
+}
+const mobileProjects = document.querySelector('.project-works');
+const desktopProjects = document.querySelector('.grid-container')
+
+const projects = [mobileProjects, desktopProjects];
+for (let i = 0; i < projects.length; i++){
+	console.log(projects[i]);
+}
+
+
+
+
+// console.log(mobileProjects);
+// console.log(desktopProjects);
+
